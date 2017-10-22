f08d6b6a5f1625445e62f4b87d04b52c40f6e034
Stephen Policelli <123outerme@outlook.com>
2017-10-21 22:16:49 -0400

    Optimized independent building

--- GtL-bin/build/a_makeWindows.bat
+++ GtL-bin/build/a_makeWindows.bat
@@ -1,7 +1,9 @@
 @windres resource.rc resource.o
-gcc -w -L"%cd%\SDL" main.c SDLGateway.c SDLGateway.h outermeSDL.c outermeSDL.h resource.o -o GatewayToLegend.exe -mwindows -lSDL2 -lSDL2_image -lSDL2_ttf
+gcc -L"%cd%\SDL" main.c SDLGateway.c SDLGateway.h outermeSDL.c outermeSDL.h resource.o -o GatewayToLegend.exe -mwindows -lSDL2 -lSDL2_image -lSDL2_ttf
+gcc -L"%cd%\SDL" mainToolchain.c outermeSDL.c outermeSDL.h resource.o -o GatewayToLegendToolchain.exe -lSDL2 -lSDL2_image -lSDL2_ttf
 @rem the above line can't be muted or it won't compile
 @move /Y %~dp0\GatewayToLegend.exe %~dp0\..\execute\
+@move /Y %~dp0\GatewayToLegendToolchain.exe %~dp0\..\execute\
 @cd ..\execute
 @set execDir=%cd%
 @cd ..\build
