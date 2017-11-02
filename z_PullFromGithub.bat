504ef0205105a57d596c276beb6ac1cb9b956794
Stephen Policelli <123outerme@outlook.com>
2017-11-02 16:38:05 -0400

    Fixed pull batch script and tweaked toolchain.txt

--- z_PullFromGithub.bat
+++ z_PullFromGithub.bat
@@ -1,16 +1,22 @@
-@set folder="C:\Program Files\Git\tmp\pulls\"
+@set folder=C:\Program Files\Git\tmp\pulls
 @cd %folder%\Gateway-to-Legend
-@git remote add origin https://github.com/123outerme/Gateway-to-Legend.git
+@IF EXIST "%folder%\Gateway-to-Legend\.git" (
+	cd /d "%folder%\Gateway-to-Legend\.git"
+	for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+	cd ..
+)
+
 @git init
+@git remote add origin https://github.com/123outerme/Gateway-to-Legend.git
 git pull origin master --allow-unrelated-histories
 @ROBOCOPY "%folder%\Gateway-to-Legend" "C:\Program Files\Git\code\Gateway-to-Legend" /MIR
-IF EXIST "%folder%\Gateway-to-Legend" (
-    cd /d "%folder%\Gateway-to-Legend"
-    for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+@IF EXIST "%folder%\Gateway-to-Legend" (
+	@cd /d "%folder%\Gateway-to-Legend"
+	@for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 )
 @ROBOCOPY "C:\Program Files\Git\code\Gateway-to-Legend" %~dp0 *.c *.h *.txt *.ico *.rc *.png /MIR /XD dirs .git GtL GtL-bin
 @cd "C:\Program Files\Git\code\Gateway-to-Legend"
 @git init
 @git add -A
 @cd %~dp0
-@pause Done! Press anything to continue.
+@rem @pause Done! Press anything to continue.
