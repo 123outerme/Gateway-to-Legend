de34a138ee02fda07485a64e3787dac88d5f9d2b
Stephen Policelli <123outerme@outlook.com>
2018-01-12 19:16:20 -0500

    Updated

--- z_PullFromGithub.bat
+++ z_PullFromGithub.bat
@@ -14,7 +14,7 @@ git pull origin master --allow-unrelated-histories
 	@cd /d "%folder%\Gateway-to-Legend"
 	@for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 )
-@ROBOCOPY "C:\Program Files\Git\code\Gateway-to-Legend" %~dp0 *.c *.h *.txt *.ico *.rc *.png *.bat /MIR /XD dirs .git GtL GtL-bin documentation
+@ROBOCOPY "C:\Program Files\Git\code\Gateway-to-Legend" %~dp0 *.c *.h *.txt *.ico *.rc *.png *.bat /MIR /XD dirs .git GtL GtL-bin videos
 @cd "C:\Program Files\Git\code\Gateway-to-Legend"
 @git init
 @git add -A
