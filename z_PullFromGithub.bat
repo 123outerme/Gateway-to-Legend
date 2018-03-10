a7f0bc612be3389db0bc504485c197b82602e363
Stephen Policelli <123outerme@outlook.com>
2018-03-09 23:58:45 -0500

    Updated bats to be copy/pasted

--- z_PullFromGithub.bat
+++ z_PullFromGithub.bat
@@ -1,21 +1,22 @@
 @set folder=C:\Program Files\Git\tmp\pulls
-@cd %folder%\Gateway-to-Legend
-@IF EXIST "%folder%\Gateway-to-Legend\.git" (
-	cd /d "%folder%\Gateway-to-Legend\.git"
+@set project=Gateway-to-Legend
+@cd %folder%\%project%
+@IF EXIST "%folder%\%project%\.git" (
+	cd /d "%folder%\%project%\.git"
 	for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 	cd ..
 )
 
 @git init
-@git remote add origin https://github.com/123outerme/Gateway-to-Legend.git
+@git remote add origin https://github.com/123outerme/%project%.git
 git pull origin master --allow-unrelated-histories
-@ROBOCOPY "%folder%\Gateway-to-Legend" "C:\Program Files\Git\code\Gateway-to-Legend" /MIR
-@IF EXIST "%folder%\Gateway-to-Legend" (
-	@cd /d "%folder%\Gateway-to-Legend"
+@ROBOCOPY "%folder%\%project%" "C:\Program Files\Git\code\%project%" /MIR
+@IF EXIST "%folder%\%project%" (
+	@cd /d "%folder%\%project%"
 	@for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 )
-@ROBOCOPY "C:\Program Files\Git\code\Gateway-to-Legend" %~dp0 *.c *.h *.txt *.ico *.rc *.png *.bat *.html *.css *.md /MIR /XD dirs .git GtL GtL-bin media
-@cd "C:\Program Files\Git\code\Gateway-to-Legend"
+@ROBOCOPY "C:\Program Files\Git\code\%project%" %~dp0 *.c *.h *.txt *.ico *.rc *.png *.bat *.html *.css *.md /MIR /XD dirs .git GtL GtL-bin media
+@cd "C:\Program Files\Git\code\%project%"
 @git init
 @git add -A
 @cd %~dp0
--- z_PushToGithub.bat
+++ z_PushToGithub.bat
@@ -1,5 +1,6 @@
-@ROBOCOPY %cd% "C:\Program Files\Git\code\Gateway-to-Legend" *.c *.h *.txt *.ico *.bin *.rc *.bat *.png *.ttf *.ogg *.mp4 *.html *.css *.md /MIR /XD dirs .git GtL GtL-bin media
-@cd "C:\Program Files\Git\code\Gateway-to-Legend"
+@set project=Gateway-to-Legend
+@ROBOCOPY %cd% "C:\Program Files\Git\code\%project%" *.c *.h *.txt *.ico *.bin *.rc *.bat *.png *.ttf *.ogg *.mp4 *.html *.css *.md /MIR /XD dirs .git GtL GtL-bin media
+@cd "C:\Program Files\Git\code\%project%"
 @set commitMessage=""
 @IF [%1]==[] (
 	set commitMessage=Updated
@@ -9,7 +10,7 @@
 @git init
 @git add .
 @git commit -m %commitMessage%
-@git remote add origin https://github.com/123outerme/Gateway-to-Legend
+@git remote add origin https://github.com/123outerme/%project%
 @git push origin master
 @cd %~dp0
 @pause Done! Press anything to continue.
